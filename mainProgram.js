3c0ec667869a15277f2cfb290f4a36bc8e0c17ed
gigonio01 <noahvanleemput@hotmail.com>
2024-04-25 15:57:29 +0200

    iuegiuezgf

--- mainProgram.js
+++ mainProgram.js
@@ -1,5 +1,5 @@
 const EventEmitter = require('events');
-class MyEmitter extends EventEmitter {}
+class MyEmitter extends EventEmitter { }
 const myEmitter = new MyEmitter();
 
 module.exports = myEmitter;
@@ -25,6 +25,9 @@ let state = STATES.setup
 let roundNR = 0
 let fails = 0
 let colorQueue = []
+let introQueueDone = false
+
+let outroCounter = 0
 
 var Gpio = require('onoff').Gpio
 //var pushButton = new Gpio(12, 'in')
@@ -33,6 +36,12 @@ const knopBlauw = new Gpio(21, 'in', 'rising', { debounceTimeout: 10 })
 const knopRood = new Gpio(20, 'in', 'rising', { debounceTimeout: 10 })
 const knopGroen = new Gpio(16, 'in', 'rising', { debounceTimeout: 10 })
 const knopGeel = new Gpio(26, 'in', 'rising', { debounceTimeout: 10 })
+const flasher = new Gpio(23, 'out', 'digital', { debounceTimeout: 10 })
+flasher.writeSync(1)
+
+setTimeout(() => {
+	flasher.writeSync(0)
+}, 3000)
 setupButtons()
 buttonColor = { blauw: 4, groen: 3, geel: 2, rood: 1 }
 
@@ -57,7 +66,8 @@ wss.on('connection', function connection(ws) {
 		const data = JSON.parse(message.toString())
 		console.log(JSON.stringify(data))
 		if (data.type === "queueDone" || data.type === "colorDone") {
-			if (state != STATES.fail){
+			introQueueDone = true
+			if (state !== STATES.fail && state !== STATES.intro && state !== STATES.emergencyStop) {
 				state = STATES.inGameReceivingInput
 				lastPressed = null
 			}
@@ -102,10 +112,6 @@ function setupButtons() {
 			console.log("noodstop")
 			state = STATES.emergencyStop
 			sendMessage(wss, "stop", { failCount: fails, rounds: roundNR })
-			setTimeout(()=>{
-				reset()
-				console.log("reset")
-			}, 5000)
 		}
 
 	});
@@ -147,7 +153,7 @@ function setupButtons() {
 
 function newPhase(name) {
 	currentStage = name
-	sendNewInitPhase(wss, name)
+	sendNewInitPhase(wss, name, state)
 }
 
 
@@ -156,7 +162,7 @@ function run(dvConfig) {
 		case STATES.inGameColorShowing:
 			console.log("time for funky shit")
 			roundNR++
-			stimuliCurve = 0.15 * roundNR ** 3 * fails + fails
+			stimuliCurve = 0.15 * roundNR ** 3 * Math.max(1, fails) + fails
 			const brightness = Math.min(40 + stimuliCurve * (4 * (gameMode === "HSP")), 100)
 			console.log(brightness)
 
@@ -185,22 +191,56 @@ function run(dvConfig) {
 				console.log(queueStep + "aaaaaa")
 				console.log(colorQueue)
 				if (lastColor > 6) {
-					console.log("big")
-					state = STATES.fail
-				} else if (buttonColor[lastPressed] === lastColor) {
-					queueStep++
-					console.log(queueStep + "eeeeeee")
-					console.log("correct")
-					//sendHueLampData(dvConfig, { gradient: {points: [{color: {"xy": { "x": 0.700, "y": 0.210}}}, {color: {"xy": { "x": 0.200, "y": 0.410}}} ]} }, dvConfig.ledStrips[0])
-					if (queueStep >= colorQueue.length) {
-						state = STATES.awaitingResponds
-						setTimeout(() => {
-							state = STATES.inGameColorShowing
-						}, 1000)
+					// console.log("big")
+					// state = STATES.fail
+					switch (lastColor) {
+						case 7: //appelblauwzeegroen
+							if (lastPressed === "blauw" || lastPressed === "groen") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
+						case 8: //oranje
+							if (lastPressed === "rood" || lastPressed === "geel") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
+						case 9: //rood
+							if (lastPressed === "rood") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
+						case 10: //geel
+							if (lastPressed === "geel") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
+						case 11: //groen
+							if (lastPressed === "groen") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
+						case 12: //blauw
+							if (lastPressed === "blauw") {
+								correctColor()
+							} else {
+								wrongColor()
+							}
+							break;
 					}
+				} else if (buttonColor[lastPressed] === lastColor) {
+					correctColor()
 				} else {
-					console.log("fail")
-					state = STATES.fail
+					wrongColor()
 				}
 			}
 
@@ -214,14 +254,14 @@ function run(dvConfig) {
 			flickerHueLampsFailed(dvConfig, stimuliCurve, dvConfig.lamps[0])
 			flickerHueLampsFailed(dvConfig, stimuliCurve, dvConfig.lamps[1])
 			sendMessage(wss, "fail", {})
-			
+
 			setTimeout(() => {
 				failReset()
 				state = STATES.inGameColorShowing
 			}, 4000)
-			
+
 			console.log("incorrect")
-			
+
 			state = STATES.awaitingResponds //zodat het maar een keer 
 			break;
 
@@ -236,45 +276,72 @@ function run(dvConfig) {
 
 		case STATES.intro:
 			if (lastPressed != null) {
-				sendMessage(wss, "select", { color: buttonColor[lastPressed] })
+
+				//start het spel niet als we bij de laatste stap zijn en de kleurtjes nog niet zijn gestopt
+				if (introCounter > 3 && !introQueueDone) {
+					console.log("KISS MY ASS")
+					console.log(introQueueDone)
+					console.log(introCounter)
+					break;
+				}
 
 				console.log(`introstate: ${introCounter}`)
 
 				//counter 3 -> we laten alle kleuren 3 keer zien in de color queue
-				if(introCounter === 1){
+				if (introCounter === 1) {
 					let tempQueue = []
 					for (let i = 0; i < 9; i++) {
 						tempQueue.push(i % 3 + 1)
 					}
-					sendColorQueue(wss,tempQueue)
+					sendColorQueue(wss, tempQueue)
 				}
 
+				let succes = true
 				//het is 5+1 omdat je het antwoord eigenlijk na de vraag pas krijgt, ookal is de vraag wel op introstate 5
 				if (introCounter === 3 + 1) {
 					switch (lastPressed) {
 						case "geel":
 							gameMode = "ADHD"
+							state = STATES.inGameColorShowing
 							break;
 						case "groen":
 							gameMode = "HSP"
+							state = STATES.inGameColorShowing
 							break;
 						case "rood":
 							gameMode = "ASS"
+							state = STATES.inGameColorShowing
+							break;
+						default:
+							succes = false
+							break;
 					}
 
 					console.log(`mode: ${gameMode}`)
 
-					state = STATES.inGameColorShowing
+					//state = STATES.inGameColorShowing
 				}
-				introCounter++
+				if (succes) {
+					introCounter++
+					sendMessage(wss, "select", { color: buttonColor[lastPressed] })
+				}
 			}
 
 			break;
 
-		case STATES.emergencyStop:
-			myEmitter.emit('stopFlickering');
+			case STATES.emergencyStop:
+				myEmitter.emit('stopFlickering');
+				if (lastPressed != null) {
+					sendMessage(wss, "oselect", { color: buttonColor[lastPressed] })
+					if (outroCounter === 2) {
+						reset()
+						console.log("reset")
+					}
 			
-			break
+					outroCounter++
+				}
+			
+				break
 
 		default:
 			console.log(`PANICC UNKNOWN STATE: ${state}`)
@@ -297,25 +364,47 @@ function failReset() {
 
 function newColor(stimuliCurve, lastColor) {
 	let color = Math.round((Math.random() * 3) + 1)
-	if (color === lastColor) { color = newColor(stimuliCurve, lastColor) }
 
-	if (gameMode === "ASS" && Math.round(Math.random() * 60) < stimuliCurve) {
+	if (gameMode === "ASS" && Math.round(Math.random() * 40) < stimuliCurve) {
 		//colorMath.round((Math.random() * 3) + 7)
-		color = Math.round(Math.random() * 6) + 7
+		color = Math.round(Math.random() * 5) + 7
+		console.log(color)
 	}
+
+	if (color === lastColor) { color = newColor(stimuliCurve, lastColor) }
 	return color
 }
 
-function reset(){
+function reset() {
 	roundNR = 0
 	fails = 0
-    colorQueue = []
+	colorQueue = []
 	lastPressed = null
-    queueStep = 0 //voor de input code
+	queueStep = 0 //voor de input code
 	introCounter = 0
+	introQueueDone = false
+	outroCounter = 0
 
 	gameMode = null //ADHD, HSP of ASS
 	stimuliCurve = 0
 
 	state = STATES.awaitingGameStart
+}
+
+function correctColor() {
+	queueStep++
+	console.log(queueStep + "eeeeeee")
+	console.log("correct")
+	//sendHueLampData(dvConfig, { gradient: {points: [{color: {"xy": { "x": 0.700, "y": 0.210}}}, {color: {"xy": { "x": 0.200, "y": 0.410}}} ]} }, dvConfig.ledStrips[0])
+	if (queueStep >= colorQueue.length) {
+		state = STATES.awaitingResponds
+		setTimeout(() => {
+			state = STATES.inGameColorShowing
+		}, 1000)
+	}
+}
+
+function wrongColor() {
+	console.log("fail")
+	state = STATES.fail
 }
